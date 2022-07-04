4a42c87284a535ac1d29cb4d12eaaa4c96f73638
Haseen Nurayin <78214651+haseennurayin@users.noreply.github.com>
2022-07-04 09:49:28 +0600

    Update main.py

--- main.py
+++ main.py
@@ -41,35 +41,6 @@ def login():
 
 @app.route('/pythonlogin/register', methods=['GET', 'POST'])
 def register():
-	
-	
-	
-	
-p= input("Input your password")
-x = True
-while x:  
-    if (len(p)<6 or len(p)>12):
-        break
-    elif not re.search("[a-z]",p):
-        break
-    elif not re.search("[0-9]",p):
-        break
-    elif not re.search("[A-Z]",p):
-        break
-    elif not re.search("[$#@]",p):
-        break
-    elif re.search("\s",p):
-        break
-    else:
-        print("Valid Password")
-        x=False
-        break
-
-if x:
-    print("Not a Valid Password")
-
-
-
 
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
 
