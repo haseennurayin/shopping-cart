383b8585623a982e086ad92dad60119b94f3dac2
Haseen Nurayin <78214651+haseennurayin@users.noreply.github.com>
2022-07-04 09:47:58 +0600

    Update ValidateForm.js

--- ValidateForm.js
+++ ValidateForm.js
@@ -1,47 +1,144 @@
-<html>
-<head>
-<title> Verification of valid Password </title>
-</head>
-<script>
-function verifyPassword() {
-  var pw = document.getElementById("pswd").value;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
 
-  if(pw == "") {
-     document.getElementById("message").innerHTML = "**Fill the password please!";
-     return false;
-  }
-
-  if(pw.length < 8) {
-     document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";
-     return false;
-  }
-
-  if(pw.length > 15) {
-     document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";
-     return false;
-  } else {
-     alert("Password is correct");
-  }
+public class A {
+    InputStream is;
+    PrintWriter out;
+    String INPUT = "";
+    
+    void solve()
+    {
+        String numbers = "0123456789",
+                lower_case = "abcdefghijklmnopqrstuvwxyz",
+                upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                special_characters = "!@#$%^&*()-+";
+        int n = ni();
+        char[] s = ns(n);
+        int need = 0;
+        int x = 0;
+        for(char c : s){
+            if(numbers.indexOf(c) >= 0)x |= 1;
+            if(lower_case.indexOf(c) >= 0)x |= 2;
+            if(upper_case.indexOf(c) >= 0)x |= 4;
+            if(special_characters.indexOf(c) >= 0)x |= 8;
+        }
+        need = 4-Integer.bitCount(x);
+        out.println(Math.max(8-n, need));
+    }
+    
+    void run() throws Exception
+    {
+        is = INPUT.isEmpty() ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+        out = new PrintWriter(System.out);
+        
+        long s = System.currentTimeMillis();
+        solve();
+        out.flush();
+        if(!INPUT.isEmpty())tr(System.currentTimeMillis()-s+"ms");
+    }
+    
+    public static void main(String[] args) throws Exception { new A().run(); }
+    
+    private byte[] inbuf = new byte[1024];
+    public int lenbuf = 0, ptrbuf = 0;
+    
+    private int readByte()
+    {
+        if(lenbuf == -1)throw new InputMismatchException();
+        if(ptrbuf >= lenbuf){
+            ptrbuf = 0;
+            try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+            if(lenbuf <= 0)return -1;
+        }
+        return inbuf[ptrbuf++];
+    }
+    
+    private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+    private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+    
+    private double nd() { return Double.parseDouble(ns()); }
+    private char nc() { return (char)skip(); }
+    
+    private String ns()
+    {
+        int b = skip();
+        StringBuilder sb = new StringBuilder();
+        while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+            sb.appendCodePoint(b);
+            b = readByte();
+        }
+        return sb.toString();
+    }
+    
+    private char[] ns(int n)
+    {
+        char[] buf = new char[n];
+        int b = skip(), p = 0;
+        while(p < n && !(isSpaceChar(b))){
+            buf[p++] = (char)b;
+            b = readByte();
+        }
+        return n == p ? buf : Arrays.copyOf(buf, p);
+    }
+    
+    private char[][] nm(int n, int m)
+    {
+        char[][] map = new char[n][];
+        for(int i = 0;i < n;i++)map[i] = ns(m);
+        return map;
+    }
+    
+    private int[] na(int n)
+    {
+        int[] a = new int[n];
+        for(int i = 0;i < n;i++)a[i] = ni();
+        return a;
+    }
+    
+    private int ni()
+    {
+        int num = 0, b;
+        boolean minus = false;
+        while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+        if(b == '-'){
+            minus = true;
+            b = readByte();
+        }
+        
+        while(true){
+            if(b >= '0' && b <= '9'){
+                num = num * 10 + (b - '0');
+            }else{
+                return minus ? -num : num;
+            }
+            b = readByte();
+        }
+    }
+    
+    private long nl()
+    {
+        long num = 0;
+        int b;
+        boolean minus = false;
+        while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+        if(b == '-'){
+            minus = true;
+            b = readByte();
+        }
+        
+        while(true){
+            if(b >= '0' && b <= '9'){
+                num = num * 10 + (b - '0');
+            }else{
+                return minus ? -num : num;
+            }
+            b = readByte();
+        }
+    }
+    
+    private static void tr(Object... o) { System.out.println(Arrays.deepToString(o)); }
 }
-</script>
-
-<body>
-<center>
-<h1 style="color:green">Javatpoint</h1>
-<h3> Verify valid password Example </h3>
-
-<form onsubmit ="return verifyPassword()">
-
-<td> Enter Password </td>
-<input type = "password" id = "pswd" value = ""> 
-<span id = "message" style="color:red"> </span> <br><br>
-
-
-<input type = "submit" value = "Submit">
-
-
-<button type = "reset" value = "Reset" >Reset</button>
-</form>
-</center>
-</body>
-</html>
